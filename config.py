2223574541275ca977b9fff291e120f206521fc3
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2023-10-23 14:02:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ organization_name = "Your Org Name"
 splunk_webhook_name = "Splunk-Webhook"
 splunk_webhook_url = "https://YourSpunkUrl:443/services/collector/raw"
 splunk_webhook_auth = "YourSharedSecret"
+# Optional, leave blank as "" if you want to create the payload in all networks
+network_tag = "Splunk"
